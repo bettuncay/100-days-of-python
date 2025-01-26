be0a841b35d58bd6bb2a47cbb94e92048c0bc876
bettuncay <116023734+bettuncay@users.noreply.github.com>
2025-01-26 17:26:37 +0300

    her şey küçük bir adımla başlar

--- gün-1/README.md
+++ gün-1/README.md
@@ -1,3 +1,5 @@
 # Gün 1: Python'a Giriş
 
-Bugün temel yazdırma komutlarını öğrendim.
+Bugün print(), input(),variables, variable naming gibi kavramları öğrendim. Daha sonra hepsini uygulayabileceğim bir proje yaptım. 
+Oturduğun şehre ve evcil hayvanına göre sana bir müzik grubu adı öneren program yazdım.
+Benimki "Istanbul Bear" :p
--- gün-1/untitled1.py
+++ gün-1/untitled1.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 26 14:34:56 2025
-
-@author: Casper
-"""
-
-print("Hello World!")
+# Created on Sun Jan 26 14:34:56 2025
+print("Welcome to the Band Name Generator")
+name = input("What is the name of city you grew up in?\n")
+pet = input("What's your pets name?\n")
+print("Your band name could be " + name + " " + pet)
